587d5455eda1ed221fe194de299981a7a9b26a0a
Luis Lázaro <lalazaro@keedio.com>
2015-05-06 10:38:18 +0200

    added class RegexpData

---
+++ src/main/java/org/apache/flume/regexp/RegexpData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.apache.flume.regexp;
+
+/**
+ *
+ * @author Luis Lázaro lalazaro@keedio.com
+ * Keedio
+ */
+public class RegexpData {
+
+}
