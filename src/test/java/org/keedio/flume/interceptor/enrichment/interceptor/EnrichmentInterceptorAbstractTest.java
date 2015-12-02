a314c7bd9ff8354fb75b1004c3dac10a1dd5754f
Luca Rosellini <luca.rosellini@gmail.com>
2015-12-02 12:36:34 +0100

    Extensibility refactors

---
+++ src/test/java/org/keedio/flume/interceptor/enrichment/interceptor/EnrichmentInterceptorAbstractTest.java
@@ -0,0 +1,7 @@
+package org.keedio.flume.interceptor.enrichment.interceptor;
+
+/**
+ * Created by Luca Rosellini <lrosellini@keedio.com> on 2/12/15.
+ */
+public class EnrichmentInterceptorAbstractTest {
+}
